0a000d5928025733444a0cb3746f7d1acdadc472
GavinPennock <50836070+GavinPennock@users.noreply.github.com>
2020-04-10 16:46:39 -0700

    finished module
    :+1:

--- src/_06_minion/Minion.java
+++ src/_06_minion/Minion.java
@@ -1,9 +1,37 @@
 package _06_minion;
 
 public class Minion {
-Minion(String name,int eyes, String color, String master){
-	Minion getName() {
-		System.out.println("my name is "+name);
+	private String name;
+	private int eyes;
+	private String color;
+	private String master;
+
+	public Minion(String name, int eyes, String color, String master) {
+		this.name = name;
+		this.eyes = eyes;
+		this.color = color;
+		this.master = master;
 	}
-}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getEyes() {
+		return eyes;
+	}
+
+	public String getMaster() {
+		return master;
+	}
+
+	public void setMaster(String string) {
+		this.master= string;
+
+	}
+
+	public String getColor() {
+		// TODO Auto-generated method stub
+		return color;
+	}
 }
